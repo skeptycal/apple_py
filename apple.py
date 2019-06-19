405aa7c9f4814bd3091c61fa6faec8f852a5761b
skeptycal <skeptycal@gmail.com>
2019-06-19 16:25:24 -0500

    minor

--- apple.py
+++ apple.py
@@ -6,13 +6,13 @@
 import json  # requires python >= 2.6
 import os
 import sys
-from pathlib import Path # requires python >= 3.4
+from pathlib import Path  # requires python >= 3.4
 from typing import Dict
 
 import requests
 
 # @info
-name = "apple.py"
+# __name__ = "apple.py"
 
 # @functions
 
@@ -35,7 +35,10 @@ class Project:
 
     def __init__(self,
                  init_name: str = 'test',
-                 init_description: str = 'test project'):
+                 init_description: str = 'test project',
+                 init_version: str = '0.0.1',
+                 init_filename: str = '',
+                 init_type: str = 'python_cli',):
         self.version = '0.0.1'
         self.name = init_name
         self.py_file_name = self.name + '.py'
@@ -64,13 +67,22 @@ class Project:
         if self.cli:
             print(self.name, " version: ", self.version)
         else:
+            # if gui, show dialog
             pass
         return 'test'
 
     def help(self) -> str:
         """ Print usage and help if CLI else #TODO dialog? """
-        pass
+        if self.cli:
+            print(self.help_text)
+        else:
+            # if gui, show dialog
+            pass
+        return 'test'
 
 
 p1 = Project('test',
              'Test project to see if my automation script is working ...')
+
+print(__name__)
+__
